561c56dd66cfc5c8b1a6525c67dbc1b897fe80b6
kiddchan <kidd.chan@theqwan.com>
2025-08-20 16:48:23 +0800

    feat: 預設跑login page

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\SocialLoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
